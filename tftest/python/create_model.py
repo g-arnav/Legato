a92026b5c3b8cb2597059dc3351f5cf0f8a621d7
g-arnav <arnavbrookvale@gmail.com>
2020-10-31 19:55:24 -0700

    Working model transfer to C++

--- tftest/python/create_model.py
+++ tftest/python/create_model.py
@@ -1,15 +1,21 @@
 import numpy as np
+import sys
+
+sys.path.append("/Users/arnavgupta/Gram/Legato/frugally-deep/keras_export")
+
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
+from convert_model import convert
 
-inputs = Input(shape=(4,))
-x = Dense(5, activation='relu')(inputs)
+inputs = Input(shape=(1,))
+x = Dense(10, activation='relu')(inputs)
 predictions = Dense(3, activation='softmax')(x)
 model = Model(inputs=inputs, outputs=predictions)
 model.compile(loss='categorical_crossentropy', optimizer='nadam')
 
 model.fit(
-    np.asarray([[1, 2, 3, 4], [2, 3, 4, 5]]),
-    np.asarray([[1, 0, 0], [0, 0, 1]]), epochs=10)
+    np.asarray([[1], [2]]),
+    np.asarray([[1, 0, 0], [0, 0, 1]]), epochs=10000)
 
-model.save('keras_model.h5', include_optimizer=False)
+model.save('/Users/arnavgupta/Gram/Legato/tftest/models/keras_model.h5', include_optimizer=False)
+convert('/Users/arnavgupta/Gram/Legato/tftest/models/keras_model.h5', "/Users/arnavgupta/Gram/Legato/tftest/models/fdeep_model.json", True)
--- tftest/tftest/main.cpp
+++ tftest/tftest/main.cpp
@@ -2,8 +2,12 @@
 int main()
 {
     const auto model = fdeep::load_model("/Users/arnavgupta/Gram/Legato/tftest/models/fdeep_model.json");
-    const auto result = model.predict(
-        {fdeep::tensor(fdeep::tensor_shape(static_cast<std::size_t>(4)),
-        std::vector<float>{1, 2, 3, 4})});
-    std::cout << fdeep::show_tensors(result) << std::endl;
+    const auto result1 = model.predict(
+        {fdeep::tensor(fdeep::tensor_shape(static_cast<std::size_t>(1)),
+        std::vector<float>{1})});
+    std::cout << fdeep::show_tensors(result1) << std::endl;
+    const auto result2 = model.predict(
+        {fdeep::tensor(fdeep::tensor_shape(static_cast<std::size_t>(1)),
+        std::vector<float>{2})});
+    std::cout << fdeep::show_tensors(result2) << std::endl;
 }
